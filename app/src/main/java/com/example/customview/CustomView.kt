24806eaf798e266358d7f329b00d1dd665500a54
YueShuai <yueshuai@pupupula.com>
2019-10-28 15:04:01 +0800

    初始化项目

---
+++ app/src/main/java/com/example/customview/CustomView.kt
@@ -0,0 +1,44 @@
+package com.example.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * @author YueShuai
+ * @date 2019-10-28
+ * @Describe
+ *
+ * <p>
+ * Email : yueshuai@pupupula.com
+ */
+class CustomView : View {
+    val paint = Paint()
+
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+    }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes) {
+    }
+
+    override fun draw(canvas: Canvas?) {
+        super.draw(canvas)
+       canvas?.drawCircle(50f,50f,50f,paint)
+    }
+
+}
