db5eb8b9f44ace0e005c7b379479b933a77dc518
YueShuai <yueshuai@pupupula.com>
2019-11-06 10:37:37 +0800

    写标签布局

--- viewprocessstudy/src/main/java/com/example/viewprocessstudy/CircleView.java
+++ viewprocessstudy/src/main/java/com/example/viewprocessstudy/CircleView.java
@@ -17,16 +17,47 @@ import androidx.annotation.Nullable;
  */
 public class CircleView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private static final  float RADIUS = 80;
+    private static final float RADIUS = 80;
     private static final float PADDING = 30;
+
     public CircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int size = (int) (PADDING + RADIUS) * 2;
+        int specWithMode = MeasureSpec.getMode(widthMeasureSpec);
+        int specWithSize = MeasureSpec.getSize(widthMeasureSpec);
+        int width = resolveSize(size,widthMeasureSpec);
+        int height = resolveSize(size,heightMeasureSpec);
+        setMeasuredDimension(width, height);
+    }
+
+    private void reset(int size, int specWithMode, int specWithSize) {
+        int width;
+        switch (specWithMode) {
+            case MeasureSpec.EXACTLY:
+                width = specWithSize;
+                break;
+            case MeasureSpec.AT_MOST:
+                if (size > specWithSize) {
+                    width = specWithSize;
+                } else {
+                    width = size;
+                }
+                break;
+            case MeasureSpec.UNSPECIFIED:
+                width = size;
+                break;
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.RED);
-        canvas.drawCircle(PADDING + RADIUS,PADDING+RADIUS,RADIUS,paint);
+        canvas.drawCircle(PADDING + RADIUS, PADDING + RADIUS, RADIUS, paint);
     }
 }
---
+++ viewprocessstudy/src/main/java/com/example/viewprocessstudy/TagLayout.java
@@ -0,0 +1,111 @@
+package com.example.viewprocessstudy;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author YueShuai
+ * @date 2019-11-05
+ * @Describe <p>
+ * Email : yueshuai@pupupula.com
+ */
+public class TagLayout extends ViewGroup {
+
+    List<Rect> childrenBounds = new ArrayList<>();
+
+    public TagLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthUsed = 0;
+        int heightUsed = 0;
+        int lineMaxHeight = 0;
+        int lineWidthUsed = 0;
+
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+
+            measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, heightUsed);
+
+//            if (MeasureSpec.getMode(widthMeasureSpec ) != MeasureSpec.UNSPECIFIED&&
+//                    (widthUsed = child.getMeasuredWidth()) > MeasureSpec.getSize(widthMeasureSpec)){
+            if (MeasureSpec.getMode(widthMeasureSpec) != MeasureSpec.UNSPECIFIED &&
+                    lineWidthUsed + child.getMeasuredWidth() > widthSize) {
+                lineWidthUsed = 0;
+                heightUsed += lineMaxHeight;
+                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, heightUsed);
+            }
+            Rect childBounds;
+            if (childrenBounds.size() <= i) {
+                childBounds = new Rect();
+                childrenBounds.add(childBounds);
+            } else {
+                childBounds = childrenBounds.get(i);
+            }
+            childBounds.set(lineWidthUsed, heightUsed, lineWidthUsed + child.getMeasuredWidth(), heightUsed + child.getMeasuredHeight());
+            lineWidthUsed += child.getMeasuredWidth();
+            widthUsed = Math.max(lineWidthUsed, widthUsed);
+            lineMaxHeight = Math.max(lineMaxHeight, child.getMeasuredHeight());
+        }
+
+        int measureWidth = widthUsed;
+        heightUsed += lineMaxHeight;
+        int measureHeight = heightUsed;
+        setMeasuredDimension(measureWidth, measureHeight);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+//            if (i % 2 == 0) {
+//                child.layout(0, 0, (r - l) / 2, (b - t) / 2);
+//            } else {
+//                child.layout((r - l) / 2, (b - t) / 2, r - l, b - t);
+//            }
+            Rect childBounds = childrenBounds.get(i);
+            child.layout(childBounds.left, childBounds.top, childBounds.right, childBounds.bottom);
+        }
+
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MarginLayoutParams(getContext(), attrs);
+    }
+}
+
+//layoutParams.width;
+//        layoutParams.height;
+//        child.measure(childWithSpec, childHeightSpec);
+//        Rect childBounds = childrenBouns.get(i);
+//        childrenBouns.set();
+
+//    int childWidthSpec;
+//    int childHeightSpecl;
+//    LayoutParams layoutParams = child.getLayoutParams();
+//            switch (layoutParams.width) {
+//                    case LayoutParams.MATCH_PARENT:
+//                    switch (widthMode) {
+//                    case MeasureSpec.EXACTLY:
+//                    childWidthSpec = MeasureSpec.makeMeasureSpec(widthSize - widthUsed, MeasureSpec.EXACTLY);
+//                    break;
+//                    case MeasureSpec.AT_MOST:
+//                    childWidthSpec = MeasureSpec.makeMeasureSpec(widthSize - widthUsed, MeasureSpec.EXACTLY);
+//                    break;
+//                    case MeasureSpec.UNSPECIFIED:
+//                    childWidthSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.EXACTLY);
+//                    break;
+//                    }
+//                    break;
+//                    }
